cb2a3a8428f016e8b6194b7a663da6c7d3856875
Simonne Contreras <sccontre@ucsd.edu>
2021-02-18 11:31:10 -0800

    new version

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(buttonClick);
+}
+
+function buttonClick(e) {
+	ga("send", "event", "like", "click");
 }
